1b098539f2bb76d1a3fe6d2dac6220612c43de82
ksh <ksh9409255@gmail.com>
2021-08-19 16:30:17 +0900

    delete emoticonservicetest file

--- diary/src/main/java/diaryApplication/diary/domain/emoticon/EmoticonDto.java
+++ diary/src/main/java/diaryApplication/diary/domain/emoticon/EmoticonDto.java
@@ -1,10 +1,18 @@
 package diaryApplication.diary.domain.emoticon;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class EmoticonDto {
     private String name;
     private int categoryId;
     private String description;
+
+    public EmoticonDto(String name, int categoryId, String description) {
+        this.name = name;
+        this.categoryId = categoryId;
+        this.description = description;
+    }
 }
--- diary/src/test/java/diaryApplication/diary/service/EmoticonServiceTest.java
+++
@@ -1,7 +0,0 @@
-package diaryApplication.diary.service;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class EmoticonServiceTest {
-
-}
