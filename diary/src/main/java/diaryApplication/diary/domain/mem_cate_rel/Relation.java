de66f47136728ce3d6813c6fa84fa8922068f99f
ksh <ksh9409255@gmail.com>
2021-08-19 15:44:23 +0900

    add category and memrel entity

---
+++ diary/src/main/java/diaryApplication/diary/domain/mem_cate_rel/Relation.java
@@ -0,0 +1,25 @@
+package diaryApplication.diary.domain.mem_cate_rel;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "mem_cate_rel")
+@Getter
+public class Relation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "mem_cate_rel_id",nullable = false,unique = true)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="member_id")
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="category_id")
+    @Column(name = "category_id")
+    private int categoryId;
+}
