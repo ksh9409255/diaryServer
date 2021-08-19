ba002e33f37dbb429a1b15345f588544ed6482c7
yoondeong <yoondeong23@naver.com>
2021-08-19 16:29:01 +0900

    add FriendRepository

---
+++ diary/src/main/java/diaryApplication/diary/service/FriendService.java
@@ -0,0 +1,12 @@
+package diaryApplication.diary.service;
+
+import diaryApplication.diary.repository.FriendRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FriendService {
+
+    private final FriendRepository friendRepository;
+}
