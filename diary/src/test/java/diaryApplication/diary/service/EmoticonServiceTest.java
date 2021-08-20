11e4948f5f21b3535e30155f55132c79700c1a5e
seong hwan kim <ksh9409255@gmail.com>
2021-08-20 22:41:12 +0900

    멤버에 초기 이모티콘 저장 기능 추가

--- diary/src/test/java/diaryApplication/diary/service/EmoticonServiceTest.java
+++ diary/src/test/java/diaryApplication/diary/service/EmoticonServiceTest.java
@@ -6,9 +6,11 @@ import diaryApplication.diary.domain.emoticon.Emoticon;
 import diaryApplication.diary.domain.emoticon.EmoticonDto;
 import diaryApplication.diary.domain.member.Member;
 import diaryApplication.diary.domain.member.MemberDto;
+import diaryApplication.diary.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,6 +25,9 @@ class EmoticonServiceTest {
     @Autowired
     CategoryService categoryService;
 
+    @Autowired
+    MemberRepository memberRepository;
+
     @Test
     void register() {
         CategoryDto category = new CategoryDto(1,"potato");
@@ -32,12 +37,14 @@ class EmoticonServiceTest {
     }
     @Test
     void registerMemberEmo() {
-        CategoryDto category = new CategoryDto("potato");
+        CategoryDto category = new CategoryDto(1,"potato");
         categoryService.register(category);
         EmoticonDto emoticonDto1 = new EmoticonDto("angry potato",category,"angry");
         emoticonService.register(emoticonDto1);
         EmoticonDto emoticonDto2 = new EmoticonDto("happy potato",category,"happy");
         emoticonService.register(emoticonDto2);
+        Member member = new Member();
+        memberRepository.save(member);
         emoticonService.initMemberEmoticon(1L,1);
     }
 }
